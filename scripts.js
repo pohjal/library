b022ecf1ef16191bdaa396fdb6bf5040ded8a73b
Leevi Pohja <124263697+pohjal@users.noreply.github.com>
2023-12-12 00:59:44 +0200

    add toimii jotenkin

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+const cardHolder = document.getElementById("cardHolder");
+const addButton = document.getElementById("addBook");
+addButton.addEventListener("click", createCard);
+
 const myLibrary = [];
 
 const catInAHat = new Book("Cat in a hat", 25, false, "JVG");
@@ -21,4 +25,23 @@ function Book(name, length, read, author) {
 function addBookToLibrary(book) {
   var pituus = myLibrary.length;
   myLibrary[pituus] = book;
+}
+
+function createCard() {
+  var card = document.createElement("div");
+  var name = document.createElement("p");
+  name.textContent = "Kissa";
+  var length = document.createElement("p");
+  length.textContent = 300;
+  var luettu = document.createElement("button");
+  luettu.textContent = "Not read";
+  var author = document.createElement("p");
+  author.textContent = "Jvg";
+
+  card.classList.add("card");
+  card.appendChild(name);
+  card.appendChild(length);
+  card.appendChild(luettu);
+  card.appendChild(author);
+  cardHolder.appendChild(card);
 }
